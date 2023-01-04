929759847b4133c9925d41764c709e297ad86b02
Samuel Jaja <samuel.jaja21@gmail.com>
2023-01-04 11:27:40 +0100

    Chart Style,Data Series,Data Collection

---
+++ Chapter5_2DLineCharts/ChartStyle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Chapter5_2DLineCharts
+{
+    public class ChartStyle
+    {
+        private Canvas chartCanvas;
+        private double xmin = 0;
+        private double xmax = 10;
+        private double ymin = 0;
+        private double ymax = 10;
+
+        public Canvas ChartCanvas
+        {
+            get { return chartCanvas; }
+            set { chartCanvas = value; }
+        }
+        public double Xmin
+        {
+            get { return xmin; }
+            set { xmin = value; }
+        }
+        public double Xmax
+        {
+            get { return xmax; }
+            set { xmax = value; }
+        }
+        public double Ymin
+        {
+            get { return ymin; }
+            set { ymin = value; }
+        }
+        public double Ymax
+        {
+            get { return ymax; }
+            set { ymax = value; }
+        }
+        public Point NormalizePoint(Point pt)
+        {
+            if (ChartCanvas.Width.ToString() == "NaN")
+                ChartCanvas.Width = 270;
+            if (ChartCanvas.Height.ToString() == "NaN")
+                ChartCanvas.Height = 250;
+            Point result = new Point();
+            result.X = (int)((pt.X - Xmin) * ChartCanvas.Width / (Xmax - Xmin));
+            result.Y = (int)((int)ChartCanvas.Height - (pt.Y - Ymin) * ChartCanvas.Height / (Ymax - Ymin));
+            return result;
+        }
+    }
+}
---
+++ Chapter5_2DLineCharts/LineChartExample.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chapter5_2DLineCharts
+{
+    /// <summary>
+    /// Interaction logic for LineChartExample.xaml
+    /// </summary>
+    public partial class LineChartExample : Window
+    {
+        private ChartStyle cs;
+        private DataCollection dc;
+        private DataSeries ds;
+
+        public LineChartExample()
+        {
+            InitializeComponent();
+        }
+
+        private void AddChart()
+        {
+            cs = new ChartStyle();
+            cs.ChartCanvas = chartCanvas;
+            dc = new DataCollection();
+            cs.Xmin = 0;
+            cs.Xmax = 7;
+            cs.Ymin = -1.1;
+            cs.Ymax = 1.1;
+            // Draw Sine curve: 
+            ds = new DataSeries();
+            ds.LineColor = Brushes.Blue;
+            ds.LineThickness = 2;
+            for (int i = 0; i < 50; i++)
+            {
+                double x = i / 5.0;
+                double y = Math.Sin(x);
+                ds.LineSeries.Points.Add(new Point(x, y));
+            }
+            dc.DataList.Add(ds);
+            // Draw cosine curve: 
+            ds = new DataSeries();
+            ds.LineColor = Brushes.Red;
+            ds.LinePattern = DataSeries.LinePatternEnum.DashDot;
+            ds.LineThickness = 2;
+            for (int i = 0; i < 50; i++)
+            {
+                double x = i / 5.0;
+                double y = Math.Cos(x);
+                ds.LineSeries.Points.Add(new Point(x, y));
+            }
+            dc.DataList.Add(ds);
+            dc.AddLines(cs);
+        }
+        private void chartGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            chartCanvas.Width = chartGrid.ActualWidth - 20;
+            chartCanvas.Height = chartGrid.ActualHeight - 20;
+            chartCanvas.Children.Clear();
+            AddChart();
+        }
+    }
+}
